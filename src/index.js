962a9caa9e68da08f3c9113f5bb535e44d3aed9f
PavelVlach <vlach1989@gmail.com>
2022-05-31 08:36:39 +0200

    add exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import ExpandableLabel from './components/ExpandableLabel';
+import ExpandableLabel, {
+	ExpandableLabelHeader,
+	ExpandableLabelBody,
+} from './components/ExpandableLabel';
+import ExpandableLabelsContainer from './components/ExpandableLabel/ExpandableLabelsContainer';
 import Icon from './components/Icon';
 import IconTool from './components/IconTool';
 import ModalWindow from './components/ModalWindow';
@@ -6,10 +10,14 @@ import NothingFiltered from './components/NothingFiltered';
 import OpacitySlider from './components/OpacitySlider';
 import Switcher from './components/Switcher';
 import Tag from './components/Tag';
+import TagsContainer from './components/Tag/TagsContainer';
 import Toggle from './components/Toggle';
 
 export {
 	ExpandableLabel,
+	ExpandableLabelBody,
+	ExpandableLabelHeader,
+	ExpandableLabelsContainer,
 	Icon,
 	IconTool,
 	ModalWindow,
@@ -17,5 +25,6 @@ export {
 	OpacitySlider,
 	Switcher,
 	Tag,
+	TagsContainer,
 	Toggle,
 };
