0a9870b7ad51ad2dc9d1b73bb04ef38e6f7ff76b
Kryštof <me@krystofvavra.cz>
2022-07-22 15:35:30 +0200

    decomposition to if else

--- src/components/AppStory/AppStoryBody/content/index.js
+++ src/components/AppStory/AppStoryBody/content/index.js
@@ -16,28 +16,55 @@ const AppStoryContent = ({className, children}) => {
 			sidePanelRef.current.scrollHeight - 10;
 
 		sidePanelNodes.forEach((node, index) => {
-			node.offsetTop - 100 <= event.target.scrollTop &&
-			node.offsetTop + node.offsetHeight - 100 > event.target.scrollTop
-				? // user is located in this section (node)
-				  jumpSection == null
-					? // user is scrolling
-					  userReachedBottom
-						? // user reached the bottom section
-						  setActiveSection(sidePanelNodes.length - 1)
-						: setActiveSection(index)
-					: // user jumped to a section
-					  (setActiveSection(jumpSection),
-					  userReachedBottom && jumpSection == sidePanelNodes.length - 1
-							? // user jumped to the bottom section and reached it
-							  setJumpSection(null)
-							: sidePanelNodes[jumpSection].offsetTop >
-									event.target.scrollTop - 5 &&
-							  sidePanelNodes[jumpSection].offsetTop <
-									event.target.scrollTop + 5
-							? // user reached the jumped section
-							  setJumpSection(null)
-							: null)
-				: null;
+			let userReachedSection =
+				node.offsetTop - 100 <= event.target.scrollTop &&
+				node.offsetTop + node.offsetHeight - 100 > event.target.scrollTop;
+
+			if (userReachedSection) {
+				// user is located in this section (node)
+				if (jumpSection == null) {
+					// user is scrolling
+					if (userReachedBottom) {
+						setActiveSection(sidePanelNodes.length - 1);
+					} else {
+						setActiveSection(index);
+					}
+				} else {
+					setActiveSection(jumpSection);
+					let userJumpedToBottom = jumpSection == sidePanelNodes.length - 1;
+					let userReachedJumpedSection =
+						sidePanelNodes[jumpSection].offsetTop >
+							event.target.scrollTop - 5 &&
+						sidePanelNodes[jumpSection].offsetTop < event.target.scrollTop + 5;
+
+					if (userReachedBottom && userJumpedToBottom) {
+						setJumpSection(null);
+					} else if (userReachedJumpedSection) {
+						setJumpSection(null);
+					}
+				}
+			}
+			// userReachedSection
+			// 	? // user is located in this section (node)
+			// 	  jumpSection == null
+			// 		? // user is scrolling
+			// 		  userReachedBottom
+			// 			? // user reached the bottom section
+			// 			  setActiveSection(sidePanelNodes.length - 1)
+			// 			: setActiveSection(index)
+			// 		: // user jumped to a section
+			// 		  (setActiveSection(jumpSection),
+			// 		  userReachedBottom && jumpSection == sidePanelNodes.length - 1
+			// 				? // user jumped to the bottom section and reached it
+			// 				  setJumpSection(null)
+			// 				: sidePanelNodes[jumpSection].offsetTop >
+			// 						event.target.scrollTop - 5 &&
+			// 				  sidePanelNodes[jumpSection].offsetTop <
+			// 						event.target.scrollTop + 5
+			// 				? // user reached the jumped section
+			// 				  setJumpSection(null)
+			// 				: null)
+			// 	: null;
 		});
 	};
 
