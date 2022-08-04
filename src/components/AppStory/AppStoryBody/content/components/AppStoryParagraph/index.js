3a1197de9198358e12230a534dc0ec80edc73593
Kryštof <me@krystofvavra.cz>
2022-08-04 10:22:07 +0200

    allow children in AppStory headline and subheadline

--- src/components/AppStory/AppStoryBody/content/components/AppStoryParagraph/index.js
+++ src/components/AppStory/AppStoryBody/content/components/AppStoryParagraph/index.js
@@ -3,21 +3,16 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
-const AppStoryParagraph = ({className, text, children}) => {
+const AppStoryParagraph = ({className, children}) => {
 	const classes = classnames('ptr-AppStoryParagraph', {}, className);
 
-	return (
-		<p className={classes}>
-			{text}
-			{children}
-		</p>
-	);
+	return <p className={classes}>{children ? children : null}</p>;
 };
 
 AppStoryParagraph.propTypes = {
 	className: PropTypes.string,
 	text: PropTypes.string,
-	children: PropTypes.any,
+	children: PropTypes.node,
 };
 
 export default AppStoryParagraph;
--- src/components/AppStory/AppStoryBody/content/components/AppStorySubheadline/index.js
+++ src/components/AppStory/AppStoryBody/content/components/AppStorySubheadline/index.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
-const AppStorySubheadline = ({className, subheadline}) => {
+const AppStorySubheadline = ({className, children}) => {
 	const classes = classnames('ptr-AppStorySubheadline', {}, className);
-	return <h2 className={classes}>{subheadline}</h2>;
+	return <h2 className={classes}>{children ? children : null}</h2>;
 };
 
 AppStorySubheadline.propTypes = {
 	className: PropTypes.string,
-	subheadline: PropTypes.string,
+	children: PropTypes.node,
 };
 
 export default AppStorySubheadline;
