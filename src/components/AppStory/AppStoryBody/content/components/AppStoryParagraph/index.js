d47a6be47d704c9b83d327329c96e6502ffce639
Kryštof <me@krystofvavra.cz>
2022-08-04 09:50:29 +0200

    allow children in AppStory paragraphs

--- src/components/AppStory/AppStoryBody/content/components/AppStoryParagraph/index.js
+++ src/components/AppStory/AppStoryBody/content/components/AppStoryParagraph/index.js
@@ -3,15 +3,21 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
-const AppStoryParagraph = ({className, text}) => {
+const AppStoryParagraph = ({className, text, children}) => {
 	const classes = classnames('ptr-AppStoryParagraph', {}, className);
 
-	return <p className={classes}>{text}</p>;
+	return (
+		<p className={classes}>
+			{text}
+			{children}
+		</p>
+	);
 };
 
 AppStoryParagraph.propTypes = {
 	className: PropTypes.string,
 	text: PropTypes.string,
+	children: PropTypes.any,
 };
 
 export default AppStoryParagraph;
